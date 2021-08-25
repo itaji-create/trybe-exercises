b81195c138643640cb4697e1d5c659d4082a2ebc
Itaji Carvalho <itaji.1999@gmail.com>
2021-08-25 18:54:07 -0300

    atualiza repositorio

---
+++ fundamentos/bloco-4-introduçao-a-JavaScript/dia-1/Exercicios-course.js
@@ -0,0 +1,6 @@
+const myName = "Itaji"
+const birthCity = "Sitio Novo"
+var birthyear = 1995
+console.log(myName)
+console.log(birthCity)
+console.log(birthyear)
---
+++ fundamentos/bloco-4-introduçao-a-JavaScript/dia-1/Funçoes-logica-de-programaçao.js
@@ -0,0 +1,8 @@
+var fahrenheit=212
+var celsius=(fahrenheit-32)/1.8
+console.log("Celsius = " + celsius)
+
+var celsius=100
+celsius=100*1.8
+var fahrenheit=celsius+32
+console.log("Fahrenheit = " + fahrenheit)
