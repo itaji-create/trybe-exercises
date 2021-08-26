81e8d69e477853f5b78efab52df226ba28d44f7f
Itaji Carvalho <itaji.1999@gmail.com>
2021-08-26 01:16:32 -0300

    adiciona exercicios do 1 ao 10

---
+++ fundamentos/bloco-4-introducao-javascript/dia-1/exercises.js
@@ -0,0 +1,128 @@
+
+// //exercicio 1
+// const a = 5
+// const b = 10
+
+// console.log(a + b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a / b)
+// console.log(a % b)
+
+
+
+// //exercicio 2
+// const a = 50
+// const b = 15
+
+// if (b > a) {
+//     console.log("b é maior")
+// } else {
+//     console.log('a é maior')
+// }
+
+
+
+// //exercicio 3
+// const a = 15
+// const b = 10
+// const c = 20
+
+// if (a > b && a > c) {
+//     console.log("a é maior")
+// } else if (b > a && b > c) {
+//     console.log('b é maior')
+// } else {
+//     console.log('c é maior')
+// }
+
+
+
+// //exercicio 4   
+// const a = -10
+
+// if (a > 0) {
+//     console.log("positive")
+// } else if (a < 0) {
+//     console.log('negative')
+// } else {
+//     console.log("zero")
+// }
+
+
+
+// //exercicio 5
+// const a = 80
+// const b = 30
+// let c = 70
+
+// if (a < 0 || b < 0 || c < 0) {
+//     console.log("erro")
+// } else if (a + b + c === 180) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+
+//exercicio 6
+
+
+// //exercicio 7
+// let nota = 40
+// if (nota < 0 || nota > 100) {
+//     console.log('erro')
+// } else if (nota >= 90) {
+//     console.log('A')
+// } else if (nota >= 80) {
+//     console.log('B')
+// } else if (nota >= 70) {
+//     console.log('C')
+// } else if (nota >= 60) {
+//     console.log('D')
+// } else if (nota >= 50) {
+//     console.log('E')
+// } else {
+//     console.log('F')
+// } 
+
+
+
+//exercicio 8
+
+// const a = 8
+// const b = 15
+// const c = 23
+
+// if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+//     console.log(true)   
+// } else {
+//  console.log(false)
+
+
+
+// //exercicio 9
+// const a = 15
+// const b = 10
+// const c = 12
+
+// if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+
+
+// //exercicio 10
+// const custo = 10
+// const venda = 15
+// let montanteVendido = 1000
+// const custoTotal = custo * 1.2
+// const lucro = venda - custoTotal
+
+// if (custo < 0 || venda < 0 || montanteVendido < 0) {
+//     console.log("erro")
+// } else {
+//     console.log(lucro * montanteVendido)
+// }
