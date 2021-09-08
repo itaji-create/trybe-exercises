1d2e3cd8b7b0afcaf22bec59ad0c8a27d0c4ad11
Itaji Carvalho <itaji.1999@gmail.com>
2021-09-08 20:44:19 -0300

    adiciona exercicio 4

--- fundamentos/bloco-5-JavaScript-DOM-Eventos-Web-Storage/dia-3-eventos/main.js
+++ fundamentos/bloco-5-JavaScript-DOM-Eventos-Web-Storage/dia-3-eventos/main.js
@@ -28,7 +28,10 @@ function changeTech() {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
-
+function portifolio() {
+    window.open("https://github.com/itaji-create");
+}
+myWebpage.addEventListener('dblclick', portifolio)
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
