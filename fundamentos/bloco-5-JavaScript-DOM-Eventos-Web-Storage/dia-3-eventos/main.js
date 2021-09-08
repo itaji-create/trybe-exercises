4065ee1c247c4397a2fd4fdaf3e3f45530b7747c
Itaji Carvalho <itaji.1999@gmail.com>
2021-09-08 20:28:48 -0300

    adiciona exercicio 3

--- fundamentos/bloco-5-JavaScript-DOM-Eventos-Web-Storage/dia-3-eventos/main.js
+++ fundamentos/bloco-5-JavaScript-DOM-Eventos-Web-Storage/dia-3-eventos/main.js
@@ -3,7 +3,6 @@ const secondLi = document.getElementById('second-li');
 const thirdLi = document.getElementById('third-li');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
-
 // 1. Copie esse arquivo e edite apenas ele;
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
@@ -21,7 +20,11 @@ function changeClassTech (event) {
  
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener('keyup', changeTech);
 
+function changeTech() {
+    document.getElementsByClassName('tech')[0].innerText = input.value;
+}
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
@@ -31,11 +34,7 @@ function changeClassTech (event) {
 
 // Segue abaixo um exemplo do uso de event.target:
 
-input.addEventListener('keyup', changeTech);
 
-function changeTech(event) {
-    document.getElementsByClassName.innerText = input.value;
-}
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
