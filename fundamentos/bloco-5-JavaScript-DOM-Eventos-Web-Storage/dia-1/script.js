688780a84a07b95f6ad0564dde5e637abcf6a1a3
Itaji Carvalho <itaji.1999@gmail.com>
2021-09-28 13:19:16 -0300

    adiciona exercise/5.1

---
+++ fundamentos/bloco-5-JavaScript-DOM-Eventos-Web-Storage/dia-1/script.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName('p')[1].innerText = "Imagino estar trabalhando como dev Back-end";
+document.querySelector('.main-content').style.backgroundColor = "rgb(76,164,109)";
+document.querySelector('.center-content').style.backgroundColor = "white";
+document.querySelector('.title').innerText = "Exercício 5.1 - JavaScript";
+let text = document.getElementsByTagName('p')[1].innerText;
+document.getElementsByTagName('p')[1].innerText = text.toUpperCase();
